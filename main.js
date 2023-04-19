c58685e00b505423a39b312792ba5241d8fefff6
AlosyCrazy <103683617+AlosyCrazy@users.noreply.github.com>
2023-04-19 19:22:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -50,13 +50,13 @@ function setup() {
 function draw() {
   if(drawApple == "set")
   {
-    for(var i = 1; i <= to_number; i++)
+    for(var i = 1; i <= number; i++)
     {
       x = Math.floor(Math.random() * 700);
       y = Math.floor(Math.random() * 400);
       Image(apple, x, y, 50, 50);
     }
-    document.getElementById("status").innerHTML = toNumber + " maçãs desenhadas";
+    document.getElementById("status").innerHTML = number + " maçãs desenhadas";
     speakData = number+" maçãs desenhadas";
     speak();
     drawApple = "";
